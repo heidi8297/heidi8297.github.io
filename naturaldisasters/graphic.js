56627ff64531b34ef32525bdf3907b24661f6627
Heidi Stockton <stardust8297@gmail.com>
2021-12-12 08:28:22 -0800

    natural disasters: adding responsivefy function, updating ordering of stylesheets, adding Google fonts, updating og metadata, updating header styling

--- naturaldisasters/graphic.js
+++ naturaldisasters/graphic.js
@@ -1,3 +1,25 @@
+// this function makes our svg responsive to the size of the container/screen!
+// initial version provided by Ben Clinkinbeard and Brendan Sudol
+function responsivefy(thisSvg,maxWidth=4000) {
+  const container = d3.select(thisSvg.node().parentNode),
+    width = parseInt(thisSvg.style('width'), 10),
+    height = parseInt(thisSvg.style('height'), 10),
+    aspect = width / height;
+  thisSvg.attr('viewBox', `0 0 ${width} ${height}`)
+    .attr('preserveAspectRatio', 'xMinYMid')
+    .call(resize);
+  d3.select(window).on(
+    'resize.' + container.attr('id'),
+    resize
+  );
+  function resize() {
+    const w = Math.min(maxWidth,parseInt(container.style('width')));
+    thisSvg.attr('width', w);
+    thisSvg.attr('height', Math.round(w / aspect));
+  }
+}
+
+
 /*
 	I've created a function here that is a simple d3 chart.
 	This could be anything that has discrete steps, as simple as changing
