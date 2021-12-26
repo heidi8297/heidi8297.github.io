957c061ab14cb8a6f671079c59b9f7606c23ea83
Heidi Stockton <stardust8297@gmail.com>
2021-12-26 07:18:45 -0800

    creating a new viz-container div to contain svgs and canvas, adding two svgs one below and one above the canvas, adding a MAP to the 4th step/transition in the new backgroundSvg, adding some commented code for drawing a map on canvas that I may return to later if needed

--- naturaldisasters/disaster-main.js
+++ naturaldisasters/disaster-main.js
@@ -1,11 +1,7 @@
-/*
-	This function creates a simple d3 chart.
-	This could be anything that has discrete steps.
-	The important part is that it exposes an update function that
-	calls a new thing on a scroll trigger.
-*/
+
 window.createGraphic = function(graphicSelector) {
 	let data = [];
+	let mapData = [];
 	let eventData = [];
 	let eventsByType = [];
 	let eventTypesByDeathTolls = [];
@@ -18,19 +14,57 @@ window.createGraphic = function(graphicSelector) {
 	const canvasHeight = 2400;
 	const canvasMargin = 0.01*canvasWidth;
 
+	const vizWidth = 1000;
+	const vizHeight = 800;
+
+	const scaleFactor = canvasWidth/vizWidth;
+
 	const speedFactor = 2.2;
+
+	// create an svg which we will use for our world map / background image
+	var backgroundSvg = d3.select("#viz-container").append('svg');
+
+	var g = backgroundSvg.append('g')
+		.attr('width', vizWidth)
+		.attr('height', vizHeight);
+
+	var projection = d3.geoNaturalEarth1()
+		.scale(vizWidth / 1.4 / Math.PI)
+		.translate([-30+ vizWidth / 2, vizHeight / 2]);
+	var geoPath = d3.geoPath(projection);
+
+	// draw the map and set the opacity to 0
+	d3.json("world.geojson").then( function(worldData){
+	  g.selectAll('path')
+	    .data(worldData.features)
+	    .join('path')
+	    .attr('fill', '#EAE0DB')
+			.attr('opacity', 0)
+	    .attr('d', geoPath);
+	});
+
 
 	// create variables for referring to the 'canvas' element in HTML and to its CONTEXT
 	//   the latter of which will be used for rendering our elements to the canvas
-	var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
+	var canvas = d3.select('#viz-container')  .append('canvas')  .attr('width', canvasWidth)  .attr('height', canvasHeight) ;
+	//var canvas = document.getElementById('myCanvas')
+	//var canvas = document.getElementById('canvas');
+	var ctx = canvas.node().getContext('2d');
+
 
 	// create a 'custom' element that will be part of a 'virtual' DOM
 	//   we will use this to bind our data without cluttering the actual DOM
 	var detachedContainer = document.createElement("custom");
 	var dataContainer = d3.select(detachedContainer);
 
-	const offset = 1300;  // using this for testing purposes only - can delete later
+	// Set a projection for the map. Projection = transform a lat/long on a position on the 2d map.
+	// const projection = d3.geoNaturalEarth1()
+	//     .scale(canvasWidth / 1.3 / Math.PI)
+	//     .translate([canvasWidth / 2, canvasHeight / 2])
+	// const pathGenerator = d3.geoPath(projection, ctx);
+
+	// create an svg which we will use for axes and/or other plotting needs
+	var foregroundSvg = d3.select("#viz-container").append('svg');
 
 	// define a sort order by disaster type, to be used in stacked circle chart
 	const typeSortNum = d3.scaleOrdinal()
@@ -65,6 +99,9 @@ window.createGraphic = function(graphicSelector) {
 		}, // step1()
 
 		function step2() {
+			g.selectAll("path").transition()
+				.duration(speedFactor*800)
+				.attr('opacity',0)
 			databind23(eventsByYearFlat);
 			var t = d3.timer(function(elapsed) {
 				drawEventCircles();
@@ -73,12 +110,22 @@ window.createGraphic = function(graphicSelector) {
 		}, // step2()
 
 		function step3() {
-			databind23(eventsByYearFlat, 37000);
+			g.selectAll("path").transition()
+				.duration(speedFactor*800)
+				.attr('opacity',0.8)
+			databind24(eventsByYearFlat, 37000);
 			var t = d3.timer(function(elapsed) {
 				drawEventCircles();
 				if (elapsed > speedFactor*850) t.stop();
 			}); // Timer running the draw function repeatedly for 850 ms.
-		} // step3()
+		}, // step3()
+
+		// function step4() {
+		// 	var t = d3.timer(function(elapsed) {
+		// 		drawMap(mapData);
+		// 		if (elapsed > speedFactor*850) t.stop();
+		// 	});
+		// } // step4()
 
 	] // steps
 
@@ -174,7 +221,28 @@ window.createGraphic = function(graphicSelector) {
 					} else { return 0.6 }
 				})
 				.attr("fillStyle", d => typeColor(d.disastertype) )
-	} // databind22()
+	} // databind23()
+
+	function databind24(dataToBind, deathMin=0) {  // deaths by year
+		var boundElements = dataContainer.selectAll("custom.eventCircle")
+			.data(dataToBind)
+			.join("custom")
+				.attr("class", "eventCircle")
+				.transition()
+				.ease(d3.easeQuadInOut)
+				.duration(speedFactor*800)
+				.attr("cx", d => scaleFactor*projection([d.longitude,d.latitude])[0] )
+				.attr("cy", function(d) {
+					if (d.deaths < deathMin) {
+						return canvasHeight*4 // for anything not in the top 15 events, fly off screen
+					} else {
+						return scaleFactor*projection([d.longitude,d.latitude])[1]
+					}
+				})
+				.attr("r", d => 10*Math.sqrt(d.geoIdCount) )
+				.attr("opacity", d => 0.6)
+				.attr("fillStyle", d => typeColor(d.disastertype) )
+	} // databind24()
 
 
 	function drawEventCircles() {
@@ -190,6 +258,16 @@ window.createGraphic = function(graphicSelector) {
 			ctx.closePath()
 		})
 	} // drawEventCircles()
+
+	// function drawMap(providedData) {
+	// 	ctx.clearRect(0,0,canvasWidth,canvasHeight);
+	// 	ctx.beginPath();
+	// 	pathGenerator(providedData);
+	// 	ctx.fillStyle = "#EFE8E4";
+	// 	ctx.fill();
+	// 	ctx.strokeStyle = "#EAE0DB";
+	// 	ctx.stroke()
+	// } // drawMap()
 
 	function init() {
 		setupCharts()
@@ -278,9 +356,14 @@ window.createGraphic = function(graphicSelector) {
 			eventsByYearFlat = eventsByYearFlat.concat(eventsByYear.get(key))
 		}
 
+		// Load geojson data
+		d3.json("world.geojson").then( function(disData){
+		  mapData = disData;
+			init()
+		})
 
-		init()
 	})   // end d3.csv.then
+
 
 	return {
 		update: update,
